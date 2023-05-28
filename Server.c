06d736cf33d2bda7b04a6c298a4f4b0c6c52cd0e
JohnMorgan1234 <jcarlosgl96@protonmail.com>
2023-05-28 11:01:43 +0200

    commit

---
+++ Server.c
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+#include <process.h>
+#include <direct.h>
+#include <dirent.h>
+#include <io.h>
+#include <windows.h>
+
+#define MAX_CLIENTS 30
+#define MAX_COMMAND_SIZE 1024
+#define MAX_RESPONSE_SIZE 4096
+
+void receiveFile(SOCKET clientSocket, const char* filename);
+void sendFile(SOCKET clientSocket, const char* filename);
+void handleClientCommand(SOCKET clientSocket, const char* command);
+void executePowerShellCommand(SOCKET clientSocket, const char* command);
+
+DWORD WINAPI ClientThread(LPVOID lpParam) {
+    SOCKET clientSocket = *((SOCKET*)lpParam);
+    char command[MAX_COMMAND_SIZE] = {0};
+
+    while (1) {
+        int bytesRead = recv(clientSocket, command, sizeof(command), 0);
+        if (bytesRead <= 0) {
+            printf("Cliente desconectado: %d\n", clientSocket);
+            break;
+        } else {
+            printf("Comando recibido del cliente %d: %s\n", clientSocket, command);
+            handleClientCommand(clientSocket, command);
+        }
+    }
+
+    closesocket(clientSocket);
+    return 0;
+}
+
+void handleClientCommand(SOCKET clientSocket, const char* command) {
+    char response[MAX_RESPONSE_SIZE] = {0};
+
+    if (strncmp(command, "cd ", 3) == 0) {
+        // Comando "cd": Cambiar directorio
+        const char* path = command + 3; // Ignorar el "cd " al principio del comando
+        int result = _chdir(path); // Usar _chdir en lugar de chdir
+        if (result == 0) {
+            strcpy(response, "Directorio cambiado correctamente");
+        } else {
+            strcpy(response, "Error al cambiar el directorio");
+        }
+    } else if (strncmp(command, "put ", 4) == 0) {
+        // Comando "put": Enviar archivo al servidor
+        const char* filename = command + 4; // Ignorar el "put " al principio del comando
+        sendFile(clientSocket, filename);
+        strcpy(response, "Archivo enviado correctamente");
+    } else if (strncmp(command, "get ", 4) == 0) {
+        // Comando "get": Descargar archivo del servidor
+        const char* filename = command + 4; // Ignorar el "get " al principio del comando
+        receiveFile(clientSocket, filename);
+        strcpy(response, "Archivo recibido correctamente");
+    } else if (strncmp(command, "exec ", 5) == 0) {
+        // Comando "exec": Ejecutar comando de PowerShell
+        const char* powerShellCommand = command + 5; // Ignorar el "exec " al principio del comando
+        executePowerShellCommand(clientSocket, powerShellCommand);
+        return; // No enviar una respuesta inmediata, ya que la respuesta de PowerShell se enviará por separado
+    } else if (strncmp(command, "ls", 2) == 0) {
+        // Comando "ls": Listar archivos y directorios en el directorio actual
+        DIR* dir;
+        struct dirent* entry;
+
+        dir = opendir(".");
+        if (dir == NULL) {
+            strcpy(response, "Error al abrir el directorio");
+        } else {
+            while ((entry = readdir(dir)) != NULL) {
+                strcat(response, entry->d_name);
+                strcat(response, "\n");
+            }
+            closedir(dir);
+        }
+    } else {
+        // Comando desconocido
+        strcpy(response, "Comando desconocido");
+    }
+
+    // Enviar la respuesta al cliente
+    if (send(clientSocket, response, strlen(response), 0) == SOCKET_ERROR) {
+        printf("Error al enviar la respuesta al cliente %d\n", clientSocket);
+    }
+}
+
+void receiveFile(SOCKET clientSocket, const char* filename) {
+    FILE* file = fopen(filename, "rb");  // Modo lectura binaria en lugar de escritura binaria
+    if (file == NULL) {
+        printf("Error al abrir el archivo para lectura: %s\n", filename);
+        return;
+    }
+    char buffer[4096];
+    int bytesRead;
+
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        fwrite(buffer, 1, bytesRead, file);
+    }
+
+    fclose(file);
+}
+
+void sendFile(SOCKET clientSocket, const char* filename) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("Error al abrir el archivo para lectura: %s\n", filename);
+        return;
+    }
+
+    char buffer[4096];
+    int bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        if (send(clientSocket, buffer, bytesRead, 0) == SOCKET_ERROR) {
+            printf("Error al enviar el archivo al cliente %d\n", clientSocket);
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+void executePowerShellCommand(SOCKET clientSocket, const char* command) {
+    // Código para ejecutar el comando de PowerShell y enviar la salida al cliente
+    // Aquí puedes usar cualquier método que te permita ejecutar comandos de PowerShell y obtener la salida
+    
+    // Ejemplo de código:
+    FILE* pipe = _popen(command, "r");
+    if (pipe == NULL) {
+        printf("Error al ejecutar el comando de PowerShell: %s\n", command);
+        return;
+    }
+
+    char buffer[4096];
+    int bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), pipe)) > 0) {
+        if (send(clientSocket, buffer, bytesRead, 0) == SOCKET_ERROR) {
+            printf("Error al enviar la salida de PowerShell al cliente %d\n", clientSocket);
+            break;
+        }
+    }
+
+    _pclose(pipe);
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+    unsigned short serverPort = 12345;
+
+    // Inicializar Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("Error al inicializar Winsock\n");
+        return 1;
+    }
+
+    // Crear el socket del servidor
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        printf("Error al crear el socket del servidor\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // Configurar la dirección del servidor
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(serverPort);
+
+    // Vincular el socket a la dirección del servidor
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        printf("Error al vincular el socket a la dirección del servidor\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Escuchar conexiones entrantes
+    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+        printf("Error al escuchar conexiones entrantes\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Servidor esperando conexiones en el puerto %d...\n", serverPort);
+
+    while (1) {
+        int clientAddressSize = sizeof(clientAddress);
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressSize);
+        if (clientSocket == INVALID_SOCKET) {
+            printf("Error al aceptar la conexión del cliente\n");
+            closesocket(serverSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        printf("Cliente conectado: %d\n", clientSocket);
+
+        // Crear un hilo para manejar la comunicación con el cliente
+        HANDLE clientThread = CreateThread(NULL, 0, ClientThread, &clientSocket, 0, NULL);
+        if (clientThread == NULL) {
+            printf("Error al crear el hilo para el cliente %d\n", clientSocket);
+            closesocket(clientSocket);
+        } else {
+            // Liberar el hilo cuando haya terminado
+            CloseHandle(clientThread);
+        }
+    }
+
+    // Cerrar el socket del servidor
+    closesocket(serverSocket);
+
+    // Limpiar Winsock
+    WSACleanup();
+
+    return 0;
+}
---
+++ linux_client.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAX_COMMAND_SIZE 1024
+#define MAX_RESPONSE_SIZE 4096
+#define MAX_FILE_NAME 256
+
+void showHelp() {
+    printf("Comandos disponibles:\n");
+    printf("    cd <directorio>          - Cambiar de directorio en el servidor\n");
+    printf("    pwd                      - Mostrar el directorio actual en el servidor\n");
+    printf("    ls                       - Listar archivos y directorios en el servidor\n");
+    printf("    put <archivo_local>      - Subir un archivo al servidor\n");
+    printf("    get <archivo_remoto>     - Descargar un archivo del servidor\n");
+    printf("    exec <comando_powershell> - Ejecutar un comando de PowerShell en el servidor\n");
+    printf("    exit                     - Salir del cliente\n");
+}
+
+int sendCommand(int socket, const char* command) {
+    if (send(socket, command, strlen(command), 0) < 0) {
+        printf("Error al enviar el comando\n");
+        return -1;
+    }
+    return 0;
+}
+
+int receiveResponse(int socket, char* response) {
+    memset(response, 0, MAX_RESPONSE_SIZE);
+    if (recv(socket, response, MAX_RESPONSE_SIZE, 0) < 0) {
+        printf("Error al recibir la respuesta\n");
+        return -1;
+    }
+    return 0;
+}
+
+int sendFile(int socket, const char* fileName) {
+    FILE* file = fopen(fileName, "rb");
+    if (file == NULL) {
+        printf("Error al abrir el archivo para lectura\n");
+        return -1;
+    }
+
+    char buffer[MAX_RESPONSE_SIZE];
+    int bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        if (send(socket, buffer, bytesRead, 0) < 0) {
+            printf("Error al enviar el archivo\n");
+            fclose(file);
+            return -1;
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+
+int receiveFile(int socket, const char* fileName) {
+    FILE* file = fopen(fileName, "wb");
+    if (file == NULL) {
+        printf("Error al abrir el archivo para escritura\n");
+        return -1;
+    }
+
+    char buffer[MAX_RESPONSE_SIZE];
+    int bytesRead;
+    while ((bytesRead = recv(socket, buffer, sizeof(buffer), 0)) > 0) {
+        fwrite(buffer, 1, bytesRead, file);
+    }
+
+    fclose(file);
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <ip_servidor>\n", argv[0]);
+        return 1;
+    }
+
+    char* serverIP = argv[1];
+    int serverPort = 12345;
+
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+
+    // Crear socket del cliente
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error al crear el socket del cliente\n");
+        return 1;
+    }
+
+    // Configurar la estructura de la dirección del servidor
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+
+    if (inet_pton(AF_INET, serverIP, &(serverAddr.sin_addr)) <= 0) {
+        printf("Dirección IP inválida\n");
+        return 1;
+    }
+
+    // Conectar al servidor
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        printf("Error al conectar al servidor\n");
+        return 1;
+    }
+
+    char command[MAX_COMMAND_SIZE];
+    char response[MAX_RESPONSE_SIZE];
+
+    printf("Cliente conectado al servidor\n");
+    showHelp();
+
+    while (1) {
+        printf("Ingrese un comando: ");
+        fgets(command, sizeof(command), stdin);
+
+        // Eliminar el salto de línea final
+        command[strcspn(command, "\n")] = 0;
+
+        // Comprobar el comando ingresado
+        if (strcmp(command, "--help") == 0) {
+            showHelp();
+            continue;
+        } else if (strcmp(command, "exit") == 0) {
+            printf("Saliendo del cliente\n");
+            break;
+        }
+
+        // Enviar el comando al servidor
+        if (sendCommand(clientSocket, command) != 0) {
+            continue;
+        }
+
+        // Recibir la respuesta del servidor
+        if (receiveResponse(clientSocket, response) != 0) {
+            continue;
+        }
+
+        // Interpretar la respuesta del servidor
+        if (strcmp(response, "READY_FOR_UPLOAD") == 0) {
+            // Enviar el archivo al servidor
+            char fileName[MAX_FILE_NAME];
+            sscanf(command, "put %s", fileName);
+            if (sendFile(clientSocket, fileName) == -1) {
+                printf("Error al enviar el archivo al servidor\n");
+            } else {
+                printf("Archivo enviado exitosamente\n");
+            }
+        } else if (strcmp(response, "READY_FOR_DOWNLOAD") == 0) {
+            // Recibir el archivo del servidor
+            char fileName[MAX_FILE_NAME];
+            sscanf(command, "get %s", fileName);
+            if (receiveFile(clientSocket, fileName) == -1) {
+                printf("Error al recibir el archivo del servidor\n");
+            } else {
+                printf("Archivo recibido exitosamente\n");
+            }
+        } else {
+            // Mostrar la respuesta del servidor
+            printf("Respuesta del servidor: %s\n", response);
+        }
+    }
+
+    // Cerrar socket del cliente
+    close(clientSocket);
+
+    return 0;
+}
